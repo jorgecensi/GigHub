70b326a5c50482654efaedc84e6c36e5d18c9e0f
Jorge Censi <jorge_censi_@hotmail.com>
2017-07-31 22:05:32 -0300

    Implement Saving a GIg

--- GigHub/GigHub/ViewModels/GigFormVielModel.cs
+++ GigHub/GigHub/ViewModels/GigFormVielModel.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 
 namespace GigHub.ViewModels
 {
-    public class GigFormVielModel
+    public class GigFormViewModel
     {
         public string Venue { get; set; }
         public string Date { get; set; }
         public string Time { get; set; }
-        public int Genre { get; set; }
+        public byte Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
     }
 }
