874d2ff099f6166c841e6ff72fad3385a160fd30
Jorge Censi <jorge_censi_@hotmail.com>
2017-08-11 21:14:02 -0300

    Implement Notifications Controller and Install AutoMapper

---
+++ GigHub/GigHub/App_Start/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using GigHub.DTOs;
+using GigHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.App_Start
+{
+    public class MappingProfile
+    {
+        public MappingProfile()
+        {
+            
+        }
+    }
+}
--- GigHub/GigHub/Global.asax.cs
+++ GigHub/GigHub/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Http;
+﻿using AutoMapper;
+using GigHub.DTOs;
+using GigHub.Models;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -9,6 +12,12 @@ namespace GigHub
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Gig, GigDto>();
+                cfg.CreateMap<Notification, NotificationDto>();
+                cfg.CreateMap<ApplicationUser, UserDto>();
+            });
             GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
