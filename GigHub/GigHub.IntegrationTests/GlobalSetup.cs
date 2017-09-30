3032f922fbc912de280c4e406c9b63d79663f134
Jorge Censi <jorge_censi_@hotmail.com>
2017-09-30 18:32:28 -0300

    Mocking Current User

--- GigHub/GigHub.IntegrationTests/GlobalSetup.cs
+++ GigHub/GigHub.IntegrationTests/GlobalSetup.cs
@@ -1,5 +1,8 @@
-﻿using NUnit.Framework;
+﻿using GigHub.Core.Models;
+using GigHub.Persistence;
+using NUnit.Framework;
 using System.Data.Entity.Migrations;
+using System.Linq;
 
 namespace GigHub.IntegrationTests
 {
@@ -9,10 +12,29 @@ namespace GigHub.IntegrationTests
         [SetUp]
         public void SetUp()
         {
+            MigrateDbToLatestVersion();
+
+            Seed();
+        }
+
+        private static void MigrateDbToLatestVersion()
+        {
             //With this lines, if we don't have a database, it wil be created and then it will be migrated to the latest version
             var configuration = new GigHub.Migrations.Configuration();
             var migrator = new DbMigrator(configuration);
             migrator.Update();
+        }
+
+        public void Seed()
+        {
+            var context = new ApplicationDbContext();
+
+            if (context.Users.Any())
+                return;
+
+            context.Users.Add(new ApplicationUser { UserName = "user1", Name = "user1", Email = "-", PasswordHash = "-" });
+            context.Users.Add(new ApplicationUser { UserName = "user2", Name = "user2", Email = "-", PasswordHash = "-" });
+            context.SaveChanges();
         }
     }
 }
